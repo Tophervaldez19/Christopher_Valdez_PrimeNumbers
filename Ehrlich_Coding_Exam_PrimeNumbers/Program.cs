2f164977ebc1311ffa97db75e5e029d6d27cb0e4
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-09-18 17:25:50 +0800

    Add project files.

---
+++ Ehrlich_Coding_Exam_PrimeNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Console.Write("Input a Word:");
+
+    var number = Convert.ToInt32(Console.ReadLine());
+
+    //var number = Convert.ToInt32(input);
+
+    if (number == 1)
+    {
+        Console.WriteLine("Neither prime nor composite.");
+        continue;
+    }
+
+    if (number <= 0)
+    {
+        Console.WriteLine("Not a Prime Number");
+        continue;
+    }
+
+    bool isPrime = true;
+
+    for (int i = 2; i < number; i++)
+    {
+        if ((number % i) == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    Console.WriteLine(isPrime ? "Prime number" : "Not a prime number");
+    Console.WriteLine("~~~~~~~~~~~~~~~");
+}
